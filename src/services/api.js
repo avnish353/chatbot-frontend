1da13f6a8ecd0526e2b540a9939c72120332551d
Avnish Sharma <avnishsharma6268@gmail.com>
2026-07-08 14:15:49 +0530

    Update API base URL to use HTTPS

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "chatbot-backend-production-fe14.up.railway.app/api",
+  baseURL: "https://chatbot-backend-production-fe14.up.railway.app/api",
 });
 
 // --------------------
