038e5e0ecd51a1a5638622583331099c59cb527f
Avnish Sharma <avnishsharma6268@gmail.com>
2026-07-08 13:31:30 +0530

    Update API base URL for production environment

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "https://chatbot-backend-lqps.onrender.com/api",
+  baseURL: "chatbot-backend-production-fe14.up.railway.app/api",
 });
 
 // --------------------
